9188ea0568b1c3ee6d60099f85bad928f439ddbb
tesfaye neway <tesfayedv@gmail.com>
2023-05-16 02:46:21 -0500

    upae the py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My parents healthy diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Healthy food 1")
+streamlit.text("Healthy food 2")
+streamlit.text("Healthy food 3")
