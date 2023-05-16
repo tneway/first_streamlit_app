abe2d903111b87cddf2962b62bf7ba50deb452cd
tesfaye neway <tesfayedv@gmail.com>
2023-05-16 03:00:46 -0500

    upae the py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas as pd
+
 streamlit.title("My parents healthy diner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado toast')
+# streamlit.header('Breakfast Menu')
+# streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+# streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+# streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+# streamlit.text('🥑🍞 Avocado toast')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
