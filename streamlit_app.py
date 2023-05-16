f57a9c4ee25bff1012d6bb981bded38c650854b9
tesfaye neway <tesfayedv@gmail.com>
2023-05-16 02:55:19 -0500

    upae the py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title("My parents healthy diner")
 
-streamlit.header('🥣Breakfast Menu')
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
