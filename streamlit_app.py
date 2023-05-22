cd92ad0b065810a727e68f1172027e3e6d7927fe
tesfaye neway <tesfayedv@gmail.com>
2023-05-22 18:48:32 -0500

    update the py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ st.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
 
