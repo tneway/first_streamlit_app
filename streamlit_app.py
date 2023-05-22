5cc23c7b17a9faddd1ccde8bcc4005f6e2b7da0e
tesfaye neway <tesfayedv@gmail.com>
2023-05-22 18:52:55 -0500

    update the py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,6 @@ st.text(my_data_row)
 
 
 my_cur.execute("select * from fruit_load_list")
-my_data_row=my_cur.fetchall()
-st.text("The fruit load list contains:")
-st.text(my_data_row)
+my_data_rows=my_cur.fetchall()
+st.header("The fruit load list contains:")
+st.dataframe(my_data_rows)
