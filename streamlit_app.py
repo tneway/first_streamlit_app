123716ce54c8b250e78692f76c2922511b87c848
tesfaye neway <tesfayedv@gmail.com>
2023-05-22 18:38:10 -0500

    update the py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 import requests
 import snowflake.connector
 
-streamlit.title("My parents healthy diner")
+st.title("My parents healthy diner")
 
 # streamlit.header('Breakfast Menu')
 # streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -19,25 +19,25 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(fruits_to_show)
+st.dataframe(fruits_to_show)
 
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
-streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+st.header("Fruityvice Fruit Advice!")
+st.text(fruityvice_response.json())
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+st.dataframe(fruityvice_normalized)
 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
