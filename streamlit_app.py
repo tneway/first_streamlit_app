b473af03b306fc19a70cf9299bfae3d62dced735
tesfaye neway <tesfayedv@gmail.com>
2023-05-22 22:10:06 -0500

    upae the py file

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,12 @@ st.dataframe(fruits_to_show)
 
 #import requests
 
+def get_fruityvice_data(fruit_choice):        
+    # write your own comment -what does the next line do? 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        # write your own comment - what does this do?
+    return fruityvice_normalized
 
 st.header("Fruityvice Fruit Advice!")
 try:
@@ -33,11 +39,8 @@ try:
     if not fruit_choice:
         st.error("please select a fruit to get information")
     else:
-        # write your own comment -what does the next line do? 
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-        # write your own comment - what does this do?
-        st.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice=)
+        st.dataframe(back_from_function)
 except URLError as e:
     st.error()
 
